94f82c7b6380583f4096f6234768c3f954b09e84
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 12:45:48 -0800

    created AnimalList function that loops through the array

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import Animal from './Animal';
 
+export default function AnimalList(props) {
+  return <div>
+    {
+      props.animals.map((animal, i) =>
+        <Animal key={`${animal.type}-${i}`} {...animal}/>)
+    }
+  </div>;
+}
