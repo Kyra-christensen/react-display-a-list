b84bfb1236ba875dbcaceaf5600cf51bd5deca49
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 11:16:04 -0800

    created dog array

--- src/dog-array.js
+++ src/dog-array.js
@@ -1,1 +1,17 @@
-
+export const dogArray = [
+  {
+    name: 'Max',
+    breed: 'Lab',
+    behavior: 'Crazy'
+  },
+  {
+    name: 'Floofy',
+    breed: 'Corgi',
+    behavior: 'Calm'
+  },
+  {
+    name: 'Gracie',
+    breed: 'Dachshund',
+    behavior: 'Playful'
+  }
+];
