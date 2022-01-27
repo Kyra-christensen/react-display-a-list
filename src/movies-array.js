98eaa4944d2aeda39a284a8597aea5c10784aee3
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 13:39:32 -0800

    created moviesArray

--- src/movies-array.js
+++ src/movies-array.js
@@ -1,1 +1,32 @@
-
+export const MoviesArray = [
+  {
+    genre: 'Action',
+    movie: [
+      {
+        name: 'Eternals',
+        year: '2021',
+        img: 'https://m.media-amazon.com/images/M/MV5BYzVjYThmNDAtOTE1NC00YjQ2LTk4NWYtNTc4Yzc4OTRhYjllXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX67_CR0,0,67,98_AL_.jpg'
+      },
+      {
+        name: 'Spider-Man: No Way Home',
+        year: '2021',
+        img: 'https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_UX67_CR0,0,67,98_AL_.jpg'
+      }
+    ]
+  },
+  {
+    genre: 'Comedy',
+    movie: [
+      {
+        name: 'Ghostbusters: Afterlife',
+        year: '2021',
+        img: 'https://m.media-amazon.com/images/M/MV5BMmZiMjdlN2UtYzdiZS00YjgxLTgyZGMtYzE4ZGU5NTlkNjhhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UX67_CR0,0,67,98_AL_.jpg'
+      },
+      {
+        name: 'The Suicide Squad',
+        year: '2021',
+        img: 'https://m.media-amazon.com/images/M/MV5BNGM3YzdlOWYtNjViZS00MTE2LWE1MWUtZmE2ZTcxZjcyMmU3XkEyXkFqcGdeQXVyODEyMTI1MjA@._V1_UX67_CR0,0,67,98_AL_.jpg'
+      }
+    ]
+  }
+];
