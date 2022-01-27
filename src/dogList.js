735139924eaaefa86497eaf65626b941420f536d
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 11:23:33 -0800

    created doglist function that loops through the dog array

--- src/dogList.js
+++ src/dogList.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import Dog from './Dog';
+export default function dogList(props) {
+  return <div>
+    {
+      props.dogs.map((dog, i) => <Dog key={`${dog.name}-${i}`} {...dog}/>)
+    }
+  </div>;
+}
 
