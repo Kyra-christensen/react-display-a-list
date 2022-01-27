2c666d6386eaff1d19c9de32cf28782e2c8e6657
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 12:40:55 -0800

    created animals array

--- src/animal-array.js
+++ src/animal-array.js
@@ -1,1 +1,20 @@
-
+export const Animals = [
+  {
+    type: 'Cow',
+    child: {
+      called: 'Calf'
+    } 
+  },
+  {
+    type: 'Cat',
+    child: {
+      called: 'Kitten'
+    }
+  },
+  {
+    type: 'Dog',
+    child: {
+      called: 'Puppy'
+    }
+  }
+];
