135ac866cf666736b4fd5cada2bd3e9e08834608
Kyra Christensen <christensen.kyral@gmail.com>
2022-01-27 14:33:37 -0800

    added styling to the movies list

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Movie from './Movie';
 
 export default function MovieList(props) {
-  return <div>
+  return <div className='movie-div'>
     {
       props.movies.map((movie, i) =>
         <Movie key={`${movie.genre}-${i}`} movie={movie.movie} genre={movie.genre}/>)
